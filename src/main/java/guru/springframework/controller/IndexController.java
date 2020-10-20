7680bdb31cd8a4583878c9a432b33943a9c5072f
JustelisJ <j.jancys@gmail.com>
2020-10-20 10:24:11 +0300

    Refactored with Project Lombok

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -5,12 +5,14 @@ import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -25,6 +27,7 @@ public class IndexController {
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
+        log.debug("Index page sent");
         return "index";
     }
 
